52f75220bdb0ac32f62d9346a5a730ae9f0338e9
sensm608 <sensm608@gmail.com>
2021-12-07 23:08:45 +0900

    DB connection

---
+++ DB_connection_file/php_connection.php
@@ -0,0 +1,15 @@
+<?php
+$con=mysqli_connect("192.168.219.104","root","pi1234","tests");
+
+
+mysqli_set_charset($con,"utf8");
+$res = mysqli_query($con, "select * from users");
+$result = array();
+
+while($row = mysqli_fetch_array($res)){
+    array_push($result,array('username'=>$row[0], 'pw'=>$row[1]));
+}
+
+echo json_encode(array("result"=>$result));
+mysqli_close($con);
+?>
